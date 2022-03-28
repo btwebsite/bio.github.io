4dd1b88da914b66e7d79fd8798fc251f00141b2e
btwebsite <101782526+btwebsite@users.noreply.github.com>
2022-03-28 17:35:28 +0700

    Add files via upload

---
+++ js/imgads.js
@@ -0,0 +1,15 @@
+var total_images = 10;
+var random_number = Math.floor((Math.random()*total_images));
+var random_img = new Array();
+
+random_img[0] = '<div class="textharga">Rp 1.300</div><a  href="https://shp.ee/w6wu44x" target="_blank"><button class="Buttonbeli" >Beli</button> </a><img src="https://cf.shopee.co.id/file/719476a48731c2281336fd44c2735818" class="size">';
+random_img[1] = '<div class="textharga">Rp 65.000</div><a href="https://shp.ee/8w9igyp" target="_blank"><button class="Buttonbeli" >Beli</button> </a><img src="https://cf.shopee.co.id/file/cca03fa20cda45ac4ca0f2f14149b2fe" class="size">';
+random_img[2] = '<div class="textharga">Rp 399.000</div><a href="https://shp.ee/a3hq3gp" target="_blank"><button class="Buttonbeli" >Beli</button> </a><img src="https://cf.shopee.co.id/file/2af4f1972d9d6e9b5f2b0d7630f06e7b" class="size">';
+random_img[3] = '<div class="textharga">Rp 110.900</div><a href="https://shp.ee/a3qv6cp" target="_blank"><button class="Buttonbeli" >Beli</button> </a><img src="https://cf.shopee.co.id/file/f16a12947e8c8bf0c01d0431922bebfd" class="size">';
+random_img[4] = '<div class="textharga">Rp 160.000</div><a href="https://shp.ee/pk9ca8p" target="_blank"><button class="Buttonbeli" >Beli</button> </a><img src="https://cf.shopee.co.id/file/4c0c9e1f2e340fcce13dbb96de68adb1" class="size">';
+random_img[5] = '<div class="textharga">Rp 489.000</div><a href="https://shp.ee/6jk22cp" target="_blank"><button class="Buttonbeli" >Beli</button> </a><img src="https://cf.shopee.co.id/file/35dcd0cfe70ccdbd014f153a9a196412" class="size">';
+random_img[6] = '<div class="textharga">Rp 8.499.000</div><a href="https://shp.ee/7ts5jyb" target="_blank"><button class="Buttonbeli" >Beli</button> </a><img src="https://cf.shopee.co.id/file/de45ae097c42b522bf3f5d72139487be" class="size">';
+random_img[7] = '<div class="textharga">Rp 35.900</div><a href="https://shp.ee/pqhbykp" target="_blank"><button class="Buttonbeli" >Beli</button> </a><img src="https://cf.shopee.co.id/file/387eb3d2e17ce76623ab1d9bfa90bbf6" class="size">';
+random_img[8] = '<div class="textharga">Rp 32.900</div><a href="https://shp.ee/tatb2np" target="_blank"><button class="Buttonbeli" >Beli</button> </a><img src="https://cf.shopee.co.id/file/7f2c24c0854d3844f39fd459d0930e26" class="size">';
+random_img[9] = '<div class="textharga">Rp 33.500</div><a href="https://shp.ee/b5mfr3p" target="_blank"><button class="Buttonbeli" >Beli</button> </a><img src="https://cf.shopee.co.id/file/531390a8c5bd5569be0b8cf40fdd066f" class="size">';
+document.write(random_img[random_number]);
